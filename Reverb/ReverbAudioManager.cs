ba00436a79b56ce21e76aca987e079370fcd7d4a
harroo <harroo@protonmail.com>
2021-12-05 20:45:38 +1100

    add basic audiomanager

---
+++ Reverb/ReverbAudioManager.cs
@@ -0,0 +1,110 @@
+
+using System;
+
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ReverbAudioManager : MonoBehaviour {
+
+    public AudioClip[] clips;
+
+    private static ReverbAudioManager instance;
+    private void Awake () {
+
+        if (instance != null) Destroy(gameObject);
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start () {
+
+        Debug.Log("[Reverb::AudioManager]: Total Pre-Loaded Audio-Clips: " + clips.Length);
+    }
+
+
+    private void PlaySound3D (string soundName, Vector3 position, float audioOverride, float pitchOverride) {
+
+        AudioClip clip = Array.Find(clips, ctx => ctx.name == soundName);
+
+        if (clip == null) {
+
+            Debug.LogWarning("[Reverb::AudioManager]: Sound `" + soundName + "' was not found!");
+
+            return;
+        }
+
+        AudioSource audioSource = new GameObject().AddComponent<AudioSource>();
+        audioSource.clip = clip;
+
+        audioSource.volume = Mathf.Clamp(
+            ReverbSettings.volume + audioOverride, 0.0f, 1.0f
+        );
+        audioSource.pitch = Mathf.Clamp(
+            UnityEngine.Random.Range(0.5f, 1.5f) + pitchOverride, -3.0f, 3.0f
+        );
+
+        audioSource.spatialBlend = 1.0f;
+        audioSource.rolloffMode = AudioRolloffMode.Linear;
+        audioSource.minDistance = 4.0f;
+        audioSource.maxDistance = 24.0f;
+        audioSource.gameObject.transform.position = position;
+
+        audioSource.gameObject.name = "Temperary Audio Object: " + soundName;
+        audioSource.Play();
+        audioSource.gameObject.AddComponent<ReverbObject>().aliveTime = clip.length + 1;
+    }
+
+    private void PlaySound (string soundName, float audioOverride, float pitchOverride) {
+
+        AudioClip clip = Array.Find(clips, ctx => ctx.name == soundName);
+
+        if (clip == null) {
+
+            Debug.LogWarning("[Reverb::AudioManager]: Sound `" + soundName + "' was not found!");
+
+            return;
+        }
+
+        AudioSource audioSource = new GameObject().AddComponent<AudioSource>();
+        audioSource.clip = clip;
+
+        audioSource.volume = Mathf.Clamp(
+            ReverbSettings.volume + audioOverride, 0.0f, 1.0f
+        );
+        audioSource.pitch = Mathf.Clamp(
+            UnityEngine.Random.Range(0.5f, 1.5f) + pitchOverride, -3.0f, 3.0f
+        );
+
+        audioSource.gameObject.name = "Temperary Audio Object: " + soundName;
+        audioSource.Play();
+        audioSource.gameObject.AddComponent<ReverbObject>().aliveTime = clip.length + 1;
+    }
+
+
+    public static void Play3D (string soundName, Vector3 position) {
+
+        instance.PlaySound3D(soundName, position, 0.0f, 0.0f);
+    }
+    public static void Play3D (string soundName, Vector3 position, float audioOverride) {
+
+        instance.PlaySound3D(soundName, position, audioOverride, 0.0f);
+    }
+    public static void Play3D (string soundName, Vector3 position, float audioOverride, float pitchOverride) {
+
+        instance.PlaySound3D(soundName, position, audioOverride, pitchOverride);
+    }
+
+    public static void Play (string soundName) {
+
+        instance.PlaySound(soundName, 0.0f, 0.0f);
+    }
+    public static void Play (string soundName, float audioOverride) {
+
+        instance.PlaySound(soundName, audioOverride, 0.0f);
+    }
+    public static void Play (string soundName, float audioOverride, float pitchOverride) {
+
+        instance.PlaySound(soundName, audioOverride, pitchOverride);
+    }
+}
---
+++ Reverb/ReverbSettings.cs
@@ -0,0 +1,9 @@
+
+//used to change the volume of reverb, to use this simply:
+//ReverbSettings.volume = 1.0f; max the volume
+//ReverbSettings.volume = 0.0f; mute the volume
+
+public static class ReverbSettings {
+
+    public static float volume;
+}
